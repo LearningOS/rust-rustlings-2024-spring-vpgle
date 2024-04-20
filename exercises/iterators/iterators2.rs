2638c435f19496811d3d4392ab3f13024aaa3e15
vpgle <42811556+vpgle@users.noreply.github.com>
2024-04-20 04:10:38 +0000

    0420-1210

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -31,7 +30,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut ret = Vec::new();
+    for i in words {
+        ret.push(capitalize_first(&i));
+    }
+    ret
 }
 
 // Step 3.
@@ -39,7 +42,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut ret = String::new();
+    for i in words {
+        ret.push_str(&capitalize_first(&i));
+    }
+    ret
 }
 
 #[cfg(test)]
