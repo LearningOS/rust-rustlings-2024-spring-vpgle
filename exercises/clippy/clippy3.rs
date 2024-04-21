323b6d0696420fa67e8ec68bcfb64d96c987a191
vpgle <42811556+vpgle@users.noreply.github.com>
2024-04-21 03:06:42 +0000

    0421-1106

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -8,14 +8,14 @@
 
 #[allow(unused_variables, unused_assignments)]
 fn main() {
-    let my_option: Option<()> = None;
-    if my_option.is_none() {
-        my_option.unwrap();
+    let my_option: Option<()> = Default::default() ;
+    if !my_option.is_none() {
+        my_option;
     }
 
     let my_arr = &[
-        -1, -2, -3
-        -4, -5, -6
+        -1, -2, -3,
+        -4, -5, -6,
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
@@ -25,7 +25,9 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    let c = value_a;
+    let d = value_b;
+    value_a = d;
+    value_b = c;
     println!("value a: {}; value b: {}", value_a, value_b);
 }
