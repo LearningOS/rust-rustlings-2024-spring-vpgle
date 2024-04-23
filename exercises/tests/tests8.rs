a7dfd880699649dcceec19839fd080c0f5d6f78c
vpgle <42811556+vpgle@users.noreply.github.com>
2024-04-23 08:44:06 +0000

    0423-1644

--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {}
 
 #[cfg(test)]
@@ -16,8 +14,8 @@ mod tests {
     use super::*;
 
     #[test]
+    #[cfg(feature = "pass")]
     fn test_success() {
-        #[cfg(feature = "pass")]
         return;
 
         panic!("no cfg set");
